6b02f60382ab1c4b37659ca6a6beef76c7ff5e0d
calebomondi <kalebmokua@gmail.com>
2026-06-24 14:41:05 +0300

    add .gitignore, remove node_modules/dist/.env from tracking

--- backend/dist/portfolio/portfolio.service.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class PortfolioService {
-    private horizonUrl;
-    private fetchXlmPrice;
-    private lookupBalance;
-    aggregate(stellarAddresses: string[]): Promise<number>;
-}
--- backend/dist/vascore/vascore.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { VascoreService } from './vascore.service';
-import type { VAScoreResponse } from './vascore.types';
-export declare class VascoreController {
-    private readonly vascoreService;
-    constructor(vascoreService: VascoreService);
-    getScores(body: {
-        stellarAddresses: string[];
-    }): Promise<VAScoreResponse>;
-}
